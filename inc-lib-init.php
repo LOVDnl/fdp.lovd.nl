ce9013b56da9f9e593901174fa5096c2dfd32193
Ivo Fokkema <Ivo@IFokkema.nl>
2023-08-02 18:35:00 +0200

    Add required library file.

---
+++ inc-lib-init.php
@@ -0,0 +1,70 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ * Adapted from /src/inc-lib-init.php in the LOVD3 project.
+ *
+ * Created     : 2023-08-02
+ * Modified    : 2023-08-02   // When modified, also change the library_version.
+ * For LOVD    : 3.0-29
+ *
+ * Copyright   : 2004-2023 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+function lovd_cleanDirName ($s)
+{
+    // Cleans a given path by resolving a relative path.
+    if (!is_string($s)) {
+        // No input.
+        return false;
+    }
+
+    // Clean up the pwd; remove '\' (some PHP versions under Windows seem to escape the slashes with backslashes???)
+    $s = stripslashes($s);
+    // Clean up the pwd; remove '//'
+    $s = preg_replace('/\/+/', '/', $s);
+    // Clean up the pwd; remove '/./'
+    $s = preg_replace('/\/\.\//', '/', $s);
+    // Clean up the pwd; remove '/dir/../'
+    $s = preg_replace('/\/[^\/]+\/\.\.\//', '/', $s);
+    // Hackers may try to give us links that start with a parent dir. That would cause an infinite loop.
+    $s = preg_replace('/^\/\.\.\//', '/', $s);
+
+    if (preg_match('/\/(\.)?\.\//', $s)) {
+        // Still not clean... Pff...
+        $s = lovd_cleanDirName($s);
+    }
+
+    return $s;
+}
+
+
+
+
+
+function lovd_getInstallURL ($bFull = true)
+{
+    // Returns URL that can be used in URLs or redirects.
+    // ROOT_PATH can be relative or absolute.
+    return (!$bFull? '' : PROTOCOL . $_SERVER['HTTP_HOST']) .
+        lovd_cleanDirName(substr(ROOT_PATH, 0, 1) == '/'? ROOT_PATH : dirname($_SERVER['SCRIPT_NAME']) . '/' . ROOT_PATH);
+}
+?>
