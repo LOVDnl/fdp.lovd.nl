60b467a68e0b2cf69997fa83cf9eeffa5605faff
Ivo Fokkema <Ivo@IFokkema.nl>
2023-08-03 18:31:00 +0200

    Add first version of the FDP API object.

---
+++ class/api.fdp.php
@@ -0,0 +1,61 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2023-08-03
+ * Modified    : 2023-08-03   // When modified, also change the library_version.
+ * For LOVD    : 3.0-29
+ *
+ * Copyright   : 2004-2023 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+// Don't allow direct access.
+if (!defined('ROOT_PATH')) {
+    exit;
+}
+
+
+
+class LOVD_API_FDP
+{
+    // This class defines the LOVD API object handling the FDP.
+
+    private $API;                     // The API object.
+
+
+
+
+
+    function __construct (&$oAPI)
+    {
+        // Links the API to the private variable.
+
+        if (!is_object($oAPI) || !is_a($oAPI, 'LOVD_API')) {
+            return false;
+        }
+        $this->API = $oAPI;
+        $this->API->aResponse['library_version'] = '2023-08-03';
+
+        return true;
+    }
+}
+?>
